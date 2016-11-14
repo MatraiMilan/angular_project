3505408c977922f3e225ec4d5757f0a4a9097204
Mátrai Milán <milan.matrai@ge.com>
2016-11-14 16:27:51 +0100

    simple angular page

--- www/js/myApp.js
+++ www/js/myApp.js
@@ -1,1 +1,13 @@
 var myApp = angular.module('myApp', []);
+myApp.controller('repeatController', function ($scope) {
+    $scope.repeatString = function () {
+
+        var x = "";
+        for (var i = 0; i < $scope.repeatNumber; i++){
+             x += $scope.textToRepeat;
+        }
+        $scope.textToRepeat = x;
+
+        console.log(x);
+    }
+})
