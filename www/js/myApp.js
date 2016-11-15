7b4bfb0af26b6a132baa34f93a9106b30d820a7a
Mátrai Milán <milan.matrai@ge.com>
2016-11-15 12:58:56 +0100

    form and basic angular

--- www/js/myApp.js
+++ www/js/myApp.js
@@ -1,13 +1,24 @@
 var myApp = angular.module('myApp', []);
-myApp.controller('repeatController', function ($scope) {
-    $scope.repeatString = function () {
-
-        var x = "";
-        for (var i = 0; i < $scope.repeatNumber; i++){
-             x += $scope.textToRepeat;
+myApp.controller('asideMenuCtrl', function ($scope, $rootScope) {
+    $rootScope.dataArray = [];
+    // console.log(element(by.binding('radio')));
+    $scope.addDataToTable = function () {
+        var money = $scope.money;
+        console.log($scope);
+        if ($scope.radio == "expense"){
+            console.log("is it run??")
+            money = money * -1;
         }
-        $scope.textToRepeat = x;
-
-        console.log(x);
+        var obj = {
+            money: money,
+            date: $scope.date,
+            desc: $scope.desc
+        };
+        console.log(obj);
+        $rootScope.dataArray.push(obj);
     }
+})
+
+myApp.controller('tableController', function () {
+
 })
